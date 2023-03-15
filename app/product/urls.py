c450324050c76cbf6a5c33d5b31124d1378f73f9
egorshurygin <127405023+egorshurygin@users.noreply.github.com>
2023-03-15 20:22:09 +0500

    Update urls.py

--- app/product/urls.py
+++ app/product/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from app.product.views import *
 
 urlpatterns = [
-    path('list/all/points', GetListAllPoints.as_view()),
-    path('list/all/display_codes', GetListAllDisplayCodeProduct.as_view()),
+    path('list/all/points', GetListAllDeviceSignals.as_view()),
+    path('list/all/display_codes', GetListAllTelegramUsers.as_view()),
     path('create/DisplayCode', CreateDisplayCode.as_view()),
 ]
